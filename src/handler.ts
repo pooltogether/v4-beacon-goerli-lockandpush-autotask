8251cc05529235195848e4f07ad38c1579792c66
Pierrick Turelier <pierrick@turelier.com>
2022-08-15 16:35:35 -0500

    feat(task): migrate to Goerli

--- src/handler.ts
+++ src/handler.ts
@@ -1,19 +1,19 @@
-import { Relayer } from 'defender-relay-client'
-import { beaconDrawLockAndNetworkTotalSupplyPush } from '@pooltogether/v4-autotask-lib'
-import { testnet as contracts } from '@pooltogether/v4-pool-data'
+import { Relayer } from 'defender-relay-client';
+import { beaconDrawLockAndNetworkTotalSupplyPush } from '@pooltogether/v4-autotask-lib';
+import { testnet as contracts } from '@pooltogether/v4-pool-data';
 
 export async function handler(event: any) {
   const relayer = new Relayer(event);
 
   const config = {
     beaconChain: {
-      chainId: 4,
-      providerUrl: event.secrets.ethereumRinkebyProviderURL,
+      chainId: 5,
+      providerUrl: event.secrets.ethereumGoerliProviderURL,
     },
     allPrizePoolNetworkChains: [
       {
-        chainId: 4,
-        providerUrl: event.secrets.ethereumRinkebyProviderURL,
+        chainId: 5,
+        providerUrl: event.secrets.ethereumGoerliProviderURL,
       },
       {
         chainId: 80001,
@@ -22,26 +22,26 @@ export async function handler(event: any) {
       {
         chainId: 69,
         providerUrl: event.secrets.optimismKovanProviderURL,
-      }
-    ]
-  }
+      },
+    ],
+  };
 
   try {
-    const transactionPopulated = await beaconDrawLockAndNetworkTotalSupplyPush(contracts, config)
+    const transactionPopulated = await beaconDrawLockAndNetworkTotalSupplyPush(contracts, config);
 
     if (transactionPopulated) {
       let transactionSentToNetwork = await relayer.sendTransaction({
         data: transactionPopulated.data,
         to: transactionPopulated.to,
         gasLimit: 500000,
-        speed: 'fast'
+        speed: 'fast',
       });
 
-      console.log('TransactionHash:', transactionSentToNetwork.hash)
+      console.log('TransactionHash:', transactionSentToNetwork.hash);
     } else {
-      throw new Error('DrawBeacon: Transaction not populated')
+      throw new Error('DrawBeacon: Transaction not populated');
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
